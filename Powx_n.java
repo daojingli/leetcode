5106bbf2e5a42ecc37e7402d4278d52e420e2bbc
Daojing Li <daojingli@gmail.com>
2016-06-20 15:53:34 +0800

    Pow(x,n)

---
+++ Powx_n.java
@@ -0,0 +1,12 @@
+/**
+ * Implement pw(x, n).
+ */
+public class Powx_n {
+    public double myPow(double x, int n) {
+        if (n == 0) return 1.0;
+        double temp = myPow(x, n / 2);
+        if (n%2 == 0) return temp * temp;
+        if (n > 0) return temp * temp * x;
+        return temp * temp / x;
+    }
+}
