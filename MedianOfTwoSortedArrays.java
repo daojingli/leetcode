c1be30606c4617ceae26cc704cf03e86bb6de445
Daojing Li <daojingli@gmail.com>
2016-06-25 13:09:44 +0800

    add SolutionII

--- MedianOfTwoSortedArrays.java
+++ MedianOfTwoSortedArrays.java
@@ -37,4 +37,38 @@ public class MedianOfTwoSortedArrays {
         
         return kth(k, a2, a1);
     }
+
+    //----------------solution II-----------------------------------------------
+
+    public double findMedianSortedArraysII(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int k = (m + n ) / 2;
+        int res = kth( k + 1, nums1, 0, m, nums2, 0, n);
+        if ((m + n)  % 2 == 0) {
+            int res2 = kth(k + 2, nums1, 0, m, nums2, 0, n);
+            return (res + res2) / 2.0;
+        }
+        return res;
+    }
+    
+    public int kth(int k, int[] a, int al, int ar, int[] b, int bl, int br) {
+        int m = ar - al;
+        int n = br - bl;
+        if (m > n) return kth(k, b, bl, br, a, al, ar);
+        System.out.println(m);
+        System.out.println(n);
+        if (m <= 0) return b[bl + k - 1];
+        if (k == 1) return Math.min(a[al], b[bl]);
+        
+        int a_mid = Math.min(m, k / 2);
+        int b_mid = k - a_mid;
+        System.out.println("a_mid item: " + a[al + a_mid - 1]);
+        System.out.println("b_mid item: " + b[bl + b_mid - 1]);
+        if (a[al + a_mid - 1] > b[bl + b_mid - 1]) {
+            return kth(k - b_mid, a, al, al + a_mid, b, bl + b_mid, br);
+        } else if (a[al + a_mid - 1] < b[bl + b_mid - 1]) {
+            return kth(k - a_mid, a, al + a_mid, ar, b, bl, bl + b_mid);
+        } else return a[al + a_mid - 1];
+    }
+
 }
